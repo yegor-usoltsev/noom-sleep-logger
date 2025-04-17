b41b313d4768cb0efa6059a407adcf97d8f0ab87
Yegor Usoltsev <yegor@usoltsev.xyz>
2025-04-17 22:51:22 +0300

    Remove PingController and associated test

--- src/main/kotlin/com/noom/interview/fullstack/sleep/App.kt
+++ src/main/kotlin/com/noom/interview/fullstack/sleep/App.kt
@@ -1,16 +1,7 @@
 package com.noom.interview.fullstack.sleep
 
-import org.jooq.DSLContext
-import org.jooq.impl.DSL.currentLocalDate
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.http.MediaType.ALL_VALUE
-import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
-import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-import java.time.LocalDate
 
 @SpringBootApplication
 class App
@@ -18,20 +9,3 @@ class App
 fun main(args: Array<String>) {
   runApplication<App>(*args)
 }
-
-@RestController
-@RequestMapping("/api/v1")
-class PingController(private val jooq: DSLContext) {
-
-  @GetMapping("/ping", consumes = [ALL_VALUE], produces = [APPLICATION_JSON_VALUE])
-  fun ping(): ResponseEntity<Pong> {
-    val dateField = currentLocalDate()
-    val date = jooq.select(dateField)
-      .fetchSingle()
-      .map { it[dateField] }
-    return ResponseEntity.ok(Pong(date))
-  }
-
-}
-
-data class Pong(val date: LocalDate)
