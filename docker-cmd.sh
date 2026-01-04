41d1cb3ae6a4b79e7c5b59371ae779e2d41d855d
Yegor Usoltsev <yegor@usoltsev.xyz>
2026-01-04 16:10:34 +0300

    Update *.sh

--- docker-cmd.sh
+++ docker-cmd.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
-set -euo pipefail
+set -eu
+
 JAVA_OPTS_DEFAULT="${JAVA_OPTS_DEFAULT:=-Duser.country=US -Duser.language=en -Duser.timezone=UTC}"
 JAVA_OPTS_ADDITIONAL="${JAVA_OPTS_ADDITIONAL:=}"
 JAVA_OPTS="${JAVA_OPTS:=${JAVA_OPTS_DEFAULT} ${JAVA_OPTS_ADDITIONAL}}"
 JAVA_ARGS="${JAVA_ARGS:=}"
+
 exec java ${JAVA_OPTS} -jar sleep.jar ${JAVA_ARGS}
