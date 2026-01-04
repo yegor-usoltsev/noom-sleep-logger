c593ef412b70c0a8a3a96e01dfa50143c2af700a
Yegor Usoltsev <yegor@usoltsev.xyz>
2026-01-04 15:28:19 +0300

    Update docker-cmd.sh

--- docker-cmd.sh
+++ docker-cmd.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-set -e
+#!/usr/bin/env sh
+set -euo pipefail
 JAVA_OPTS_DEFAULT="${JAVA_OPTS_DEFAULT:=-Duser.country=US -Duser.language=en -Duser.timezone=UTC}"
 JAVA_OPTS_ADDITIONAL="${JAVA_OPTS_ADDITIONAL:=}"
 JAVA_OPTS="${JAVA_OPTS:=${JAVA_OPTS_DEFAULT} ${JAVA_OPTS_ADDITIONAL}}"
